c011ab54d82bdf8a1b5d66af71b6441c4f15148f
Bhanu Shakya <166703471+bhanushakya2004@users.noreply.github.com>
2025-03-31 15:05:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use("/api/assignments", assignmentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
